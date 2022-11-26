095a12cded4ed1d25a6b375bbdfe2bed85c53382
Devilsbkssks <119072774+Devilsbkssks@users.noreply.github.com>
2022-11-26 14:00:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sonukumar190/link-.git /link-
+  git clone https://github.com/Devilsbkssks/link-captain.git /link-captain
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /link-
+  git clone $UPSTREAM_REPO /link-captain 
 fi
-cd /link-
+cd /link-captain 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
